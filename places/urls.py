eb36b38880ca22a7cd206682e3af9d8aa4bef8d9
andimeon <andimeon@gmail.com>
2020-07-19 17:26:52 +0900

    Removed additional urls

--- places/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('place/<int:id>', views.endpoint, name='endpoint')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
