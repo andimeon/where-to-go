392f886eccbefe16c6f4139cf43fdbe2f8c4cb6b
andimeon <andimeon@gmail.com>
2020-07-21 14:41:25 +0900

    Removed Place.place_title

--- places/models.py
+++ places/models.py
@@ -5,7 +5,6 @@ from tinymce.models import HTMLField
 
 class Place (models.Model):
     title = models.CharField('Название', max_length=200)
-    point_title = models.CharField('Точка', max_length=100)
     description_short = models.TextField('Краткое описание')
     description_long = HTMLField('Полное описание')
     lat = models.FloatField(verbose_name='Широта')
