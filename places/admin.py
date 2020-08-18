7a59d29da07931dc563aa30e040062efeb0e890b
andimeon <andimeon@gmail.com>
2020-08-18 13:33:58 +0900

    remove height of preview

--- places/admin.py
+++ places/admin.py
@@ -20,7 +20,7 @@ class ImageInline(SortableInlineAdminMixin, admin.TabularInline):
     fields = ('image', 'preview_image', 'position')
 
     def preview_image(self, instance):
-        return format_html('<img src="{0}" width="225" height="150" />', instance.image.url)
+        return format_html('<img src="{0}" width="225" />', instance.image.url)
 
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ['title']
